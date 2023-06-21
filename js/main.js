a4db34c51344c056a886ab4b000577e896f15929
Lara Gimenez <lara.gimenez@itscordoba.edu.ar>
2023-06-21 12:29:21 -0300

    creacion y modificacion de js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,54 @@
-const botones= document.querySelectorAll(".btn");
-const resultado= document.querySelector(".resultado");
+const numeros= document.querySelectorAll(".numero");
+const operadores= document.querySelectorAll(".operador");
+const borrar= document.querySelector(".borrar");
+const igual= document.querySelector(".igual");
+const pantalla= document.querySelector(".pantalla");
 
-botones.forEach(function(boton){
-    boton.addEventListener("click", function(){
-        if (boton.id === "c"){
-            resultado.textContent= "";
-            console.log(resultado.innerHTML)
-            return;
-        }
-        if (boton.id === "borrar"){
-            resultado.textContent= resultado.textContent.slice(0, -1);
-            return;
-        }
-        if (boton.id === "igual"){
-            resultado.textContent= eval(resultado.textContent);
-            return;
-        }else{
-            resultado.innerHTML += boton.innerHTML;
-        }
-        
+let opActual = "";
+let opAnterior = "";
+let resultado = "";
+let operacion = "";
+
+numeros.forEach((numero) => {
+    numero.addEventListener("click", () =>{
+        opActual += numero.innerHTML;
+        pantalla.innerHTML = opActual;
+    })
+
+})
+
+operadores.forEach((ope) => {
+    ope.addEventListener("click", () =>{   
+        opAnterior = opActual;
+        opActual = "";
+        operacion = ope.innerHTML;
     })
 })
 
+igual.addEventListener("click", () =>{  
+    switch(operacion){
+        case "+":
+            resultado = parseInt(opAnterior) + parseInt(opActual);
+            break;
+        case "-":
+            resultado = parseInt(opAnterior) - parseInt(opActual);
+            break;
+        case "x":
+            resultado = parseInt(opAnterior) * parseInt(opActual);
+            break;
+        case "÷":
+            resultado = parseInt(opAnterior) / parseInt(opActual);
+            break;
+        case "%":
+            resultado = parseInt(opAnterior) % parseInt(opActual);
+            break;
+        default:
+            console.log("Operacion no valida");
+    }
+    pantalla.innerHTML = resultado;
+})
 
-
-
-
-
-
-
-
+borrar.addEventListener("click", () =>{  
+    pantalla.innerHTML = "";
+    opActual = "";
+})
